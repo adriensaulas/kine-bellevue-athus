e908ad1d16947922ff8e3e26c5db385a6fc3b466
adriensaulas <114409621+adriensaulas@users.noreply.github.com>
2026-09-09 11:29:57 +0200

    Change base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: "/kine-bellevue-athus/",
+  base: "/",
   plugins: [react()],
 })
